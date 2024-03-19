37bdc39299c6c5a3185adfcce5f2558e86a054be
juliedosher <juliegdosher@lewisu.edu>
2024-03-18 22:57:37 -0500

    added log in from auth0

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const express = require('express')
 app = express()
-
 const cors = require("cors")
-
+const dotenv = require('dotenv');
 var url = require('url');
 const { stringify } = require('querystring');
 
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 8080
+dotenv.config()
 
 // Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
 app.use(cors({ origin: '*' }))
 
+const { auth } = require('express-openid-connect');
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: 'http://localhost:8080',
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: 'https://dev-dtw5lccxhfi7uzjg.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 
 app.get('/api/ping', (request, response) => {
